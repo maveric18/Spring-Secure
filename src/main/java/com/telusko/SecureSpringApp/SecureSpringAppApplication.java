05803da8225eda47c6b4c8970910f8fc851e0080
Sachin Raju Patil <sarajupa@tibco.com>
2023-06-20 14:29:52 +0530

    Initial Commit

---
+++ src/main/java/com/telusko/SecureSpringApp/SecureSpringAppApplication.java
@@ -0,0 +1,13 @@
+package com.telusko.SecureSpringApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecureSpringAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecureSpringAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/telusko/SecureSpringApp/SecureSpringAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.telusko.SecureSpringApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecureSpringAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
